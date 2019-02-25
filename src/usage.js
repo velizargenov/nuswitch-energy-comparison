539e86ae066c575bd2c8d91eaeb40732a243f212
Velizar Genov <velizargenov@velizargenov.local>
2019-02-25 23:34:59 +0000

    Velizar - Add tests for usage

--- src/usage.js
+++ src/usage.js
@@ -3,8 +3,38 @@ const numberOfMonthsInYear = require('./constants').numberOfMonthsInYear;
 const numberOfDaysInYear = require('./constants').numberOfDaysInYear;
 const vatRate = require('./constants').vatRate;
 
+const usage = (SUPPLIER_NAME, PLAN_NAME, SPEND) => {
+  const planData = getDataForRequestedSupplierAndPlan(data, SUPPLIER_NAME, PLAN_NAME);
+  const amountInPenceWithoutVatAndStandingCharge = getAmountInPenceWithoutVatAndStandingCharge(planData, SPEND, numberOfMonthsInYear);
+  const { rates } = planData;
+
+  const finalEnergyUsage = rates.some(rate => rate.threshold)
+    ? getAmountOfEnergyWithThresholds(rates, amountInPenceWithoutVatAndStandingCharge)
+    : getAmountOfEnergyWithoutThresholds(rates, amountInPenceWithoutVatAndStandingCharge)
+
+  console.log(finalEnergyUsage);
+  return finalEnergyUsage;
+};
+
+const getDataForRequestedSupplierAndPlan = (data, supplierName, planName) => {
+  return data.find(({ supplier, plan }) => supplier === supplierName && plan === planName);
+};
+
+const getAmountInPenceWithoutVatAndStandingCharge = (planData, spend, numberOfMonthsInYear) => {
+  const annualSpendAmountInPounds = calculateAnnualSpendAmount(spend, numberOfMonthsInYear);
+  const annualSpendAmountInPences = convertToPences(annualSpendAmountInPounds);
+  const amountWithoutVat = removeVat(annualSpendAmountInPences, vatRate);
+  const amountWithoutVatAndStandingCharge = removeStandingCharge(planData, amountWithoutVat, numberOfDaysInYear);
+
+  return amountWithoutVatAndStandingCharge;
+}
+
 const calculateAnnualSpendAmount = (monthlySpend, numberOfMonthsInYear) => {
   return monthlySpend * numberOfMonthsInYear;
+};
+
+const convertToPences = (amount) => {
+  return amount * 100;
 };
 
 const removeVat = (amount, vatRate) => {
@@ -15,15 +45,17 @@ const removeStandingCharge = ({ standing_charge }, amount, numberOfDaysInYear) =
   return standing_charge ? amount - (standing_charge * numberOfDaysInYear) : amount;
 };
 
-const convertToPences = (amount) => {
-  return amount * 100;
-};
+const getAmountOfEnergyWithThresholds = (rates, amount) => {
+  const amountOfTotalThresholdPrice = getAmountOfThreshold(rates, amount);
+  const { totalPrice, totalThreshold } = amountOfTotalThresholdPrice;
+  const tempAmount = amount - totalPrice;
+  const baseRate = rates.filter(rate => !rate.threshold)
+  const baseRateUsage = Math.round(tempAmount / baseRate[0].price);
 
-const getDataForRequestedSupplierAndPlan = (data, supplierName, planName) => {
-  return data.find(({ supplier, plan }) => supplier === supplierName && plan === planName);
-};
+  return baseRateUsage + totalThreshold;
+}
 
-var getAmountOfThreshold = (rates, amount) => {
+var getAmountOfThreshold = (rates) => {
   return rates
     .filter(rate => rate.threshold)
     .reduce((acc, currentValue) => {
@@ -40,42 +72,19 @@ var getAmountOfThreshold = (rates, amount) => {
     },0)
 }
 
-const getAmountOfEnergyWithThresholds = (rates, amount) => {
-  const amountOfTotalThresholdPrice = getAmountOfThreshold(rates, amount);
-  const { totalPrice, totalThreshold } = amountOfTotalThresholdPrice;
-  const tempAmount = amount - totalPrice;
-  const baseRate = rates.filter(rate => !rate.threshold)
-  const baseRateUsage = Math.round(tempAmount / baseRate[0].price);
-
-  return baseRateUsage + totalThreshold;
-}
-
 var getAmountOfEnergyWithoutThresholds = (rates, amount) => {
   return Math.round(amount / (rates && rates[0] && rates[0].price));
 }
 
-const getAmountInPenceWithoutVatAndStandingCharge = (planData, spend, numberOfMonthsInYear) => {
-  const annualSpendAmountInPounds = calculateAnnualSpendAmount(spend, numberOfMonthsInYear);
-  const annualSpendAmountInPences = convertToPences(annualSpendAmountInPounds);
-  const amountWithoutVat = removeVat(annualSpendAmountInPences, vatRate);
-  const amountWithoutVatAndStandingCharge = removeStandingCharge(planData, amountWithoutVat, numberOfDaysInYear);
-
-  return amountWithoutVatAndStandingCharge;
-}
-
-const usage = (SUPPLIER_NAME, PLAN_NAME, SPEND) => {
-  const planData = getDataForRequestedSupplierAndPlan(data, SUPPLIER_NAME, PLAN_NAME);
-  const amountInPenceWithoutVatAndStandingCharge = getAmountInPenceWithoutVatAndStandingCharge(planData, SPEND, numberOfMonthsInYear);
-  const { rates } = planData;
-
-  const finalEnergyUsage = rates.some(rate => rate.threshold)
-    ? getAmountOfEnergyWithThresholds(rates, amountInPenceWithoutVatAndStandingCharge)
-    : getAmountOfEnergyWithoutThresholds(rates, amountInPenceWithoutVatAndStandingCharge)
-
-  console.log(finalEnergyUsage);
-  return finalEnergyUsage;
-};
-
 module.exports = {
-  usage
+  usage,
+  getDataForRequestedSupplierAndPlan,
+  getAmountInPenceWithoutVatAndStandingCharge,
+  calculateAnnualSpendAmount,
+  convertToPences,
+  removeVat,
+  removeStandingCharge,
+  getAmountOfEnergyWithoutThresholds,
+  getAmountOfEnergyWithThresholds,
+  getAmountOfThreshold
 }
---
+++ src/usage.test.js
@@ -0,0 +1,100 @@
+const usage = require('./usage').usage;
+const getDataForRequestedSupplierAndPlan = require('./usage').getDataForRequestedSupplierAndPlan;
+const getAmountInPenceWithoutVatAndStandingCharge = require('./usage').getAmountInPenceWithoutVatAndStandingCharge;
+const calculateAnnualSpendAmount = require('./usage').calculateAnnualSpendAmount;
+const convertToPences = require('./usage').convertToPences;
+const removeVat = require('./usage').removeVat;
+const removeStandingCharge = require('./usage').removeStandingCharge;
+const getAmountOfEnergyWithoutThresholds = require('./usage').getAmountOfEnergyWithoutThresholds;
+const getAmountOfEnergyWithThresholds = require('./usage').getAmountOfEnergyWithThresholds;
+const getAmountOfThreshold = require('./usage').getAmountOfThreshold;
+
+describe('about usage', () => {
+  it('should return the expected result when the usage is invoked with the correct arguments', () => {
+    const expectedResult = 44267;
+    const supplierName = 'edf';
+    const planName = 'fixed';
+    const spend = 350;
+
+    expect(usage(supplierName, planName, spend)).toEqual(expectedResult);
+  });
+});
+
+describe('about getDataForRequestedSupplierAndPlan', () => {
+  it('should find and return the data for a plan based on supplier name and plan name', () => {
+    const data = [
+      {"supplier": "eon", "plan": "variable", "rates": [{"price": 13.5, "threshold": 100}, {"price": 10}]},
+      {"supplier": "ovo", "plan": "standard", "rates": [{"price": 12.5, "threshold": 300}, {"price": 11}]},
+      {"supplier": "edf", "plan": "fixed", "rates": [{"price": 14.5, "threshold": 250}, {"price": 10.1, "threshold": 200}, {"price": 9}]},
+      {"supplier": "bg", "plan": "standing-charge", "rates": [{"price": 9}], "standing_charge": 7}
+    ];
+    const supplierName = 'edf'
+    const planName = 'fixed';
+    const expectedResult = {"supplier": "edf", "plan": "fixed", "rates": [{"price": 14.5, "threshold": 250}, {"price": 10.1, "threshold": 200}, {"price": 9}]};
+
+    expect(getDataForRequestedSupplierAndPlan(data, supplierName, planName)).toEqual(expectedResult);
+  });
+});
+
+describe('about getAmountInPenceWithoutVatAndStandingCharge', () => {
+  it('should calculate the right amount in pence without VAT and standing charge if any specified', () => {
+    const planData = {"supplier": "edf", "plan": "fixed", "rates": [{"price": 14.5, "threshold": 250}, {"price": 10.1, "threshold": 200}, {"price": 9}]};
+    const planDataWithStandingCharge = {"supplier": "bg", "plan": "standing-charge", "rates": [{"price": 9}], "standing_charge": 7};
+    const numberOfMonthsInYear = 12;
+
+    expect(getAmountInPenceWithoutVatAndStandingCharge(planData, 350, numberOfMonthsInYear)).toEqual(400000);
+    expect(getAmountInPenceWithoutVatAndStandingCharge(planDataWithStandingCharge, 120, numberOfMonthsInYear)).toEqual(134588);
+  });
+});
+
+describe('about calculateAnnualSpendAmount', () => {
+  it('should caluclate the annual spend amount based on a monthly spend input', () => {
+    const monthlySpend = 350;
+    const numberOfMonthsInYear = 12;
+
+    expect(calculateAnnualSpendAmount(monthlySpend, numberOfMonthsInYear)).toEqual(4200);
+  });
+});
+
+describe('about convertToPences', () => {
+  it('should convert pounds to pence', () => {
+    expect(convertToPences(100)).toEqual(10000);
+  });
+});
+
+describe('about removeVat', () => {
+  it('should remove VAT from base price', () => {
+    expect(removeVat(100, 0.05)).toEqual(95);
+  });
+});
+
+describe('about removeStandingCharge', () => {
+  it('should remove standing charge if specified', () => {
+    expect(removeStandingCharge({standing_charge: 7}, 137143, 365)).toEqual(134588);
+  });
+});
+
+describe('about getAmountOfEnergyWithoutThresholds', () => {
+  it('should remove standing charge if specified', () => {
+    const rates = [{ price: 9 }];
+    const amountInPenceWithoutVatAndStandingCharge = 134588;
+    expect(getAmountOfEnergyWithoutThresholds(rates, amountInPenceWithoutVatAndStandingCharge)).toEqual(14954);
+  });
+});
+
+describe('about getAmountOfEnergyWithThresholds', () => {
+  const rates = [ { price: 14.5, threshold: 250 }, { price: 10.1, threshold: 200 }, { price: 9 } ];
+
+  it('should return the amount of energy with calculated threshoulds', () => {
+    const amount = 400000;
+    expect(getAmountOfEnergyWithThresholds(rates, amount)).toEqual(44267);
+  });
+
+  it('should return a map of accumulative total price and total threshold', () => {
+    const expectedResult = {
+      totalPrice: 5645,
+      totalThreshold: 450
+    }
+    expect(getAmountOfThreshold(rates)).toEqual(expectedResult);
+  });
+});
